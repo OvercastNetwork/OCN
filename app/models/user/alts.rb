a70038981e17c4f843d1892e89a4ff485816b187
Electroid <electroidfilms@gmail.com>
2017-08-27 16:01:55 -0700

    Temporarily disable alts until Kubernetes issue is fixed

--- app/models/user/alts.rb
+++ app/models/user/alts.rb
@@ -5,7 +5,8 @@ class User
 
         def alts
             request_cache.cache(:alts) do
-                self.class.in(mc_ips: mc_ips).reject{|u| u == self }.sort_by(&:last_seen_by).reverse
+                [] # Kubernetes normalized ips to the host ip, need to fix this with a reverse proxy
+                #self.class.in(mc_ips: mc_ips).reject{|u| u == self }.sort_by(&:last_seen_by).reverse
             end
         end
 
