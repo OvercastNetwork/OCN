bd6f8731b52ff7ca5b60ef5287b103fb1a76776e
javipepe <me@javipepe.pw>
2017-09-10 17:20:00 +0200

    Route /shop to buycraft portal

--- config/routes/shop.rb
+++ config/routes/shop.rb
@@ -1,6 +1,6 @@
 PGM::Application.routes.draw do
-	get  '/shop', :to => 'shop#index'
-	post '/shop/status', :to => 'shop#status'
-	post '/shop/purchase', :to => 'shop#purchase'
-	get  '/shop/thanks', :to => 'shop#thanks'
+	get  '/shop', :to => 'http://shop.stratus.network'
+	# post '/shop/status', :to => 'shop#status'
+	# post '/shop/purchase', :to => 'shop#purchase'
+	# get  '/shop/thanks', :to => 'shop#thanks'
 end
